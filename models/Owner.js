db2578654438b3af13e4bed1383287ee332f9dc4
AlexaP2022 <alexapunzalan@gmail.com>
2022-08-10 17:32:25 -0700

    made edits to model, seeds data, and profile handlebars html and css

--- models/Owner.js
+++ models/Owner.js
@@ -14,6 +14,7 @@ Owner.init(
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
+            autoIncrement: true
         },
         first_name: {
             type: DataTypes.STRING,
